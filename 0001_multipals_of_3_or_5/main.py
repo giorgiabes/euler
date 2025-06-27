077b6d9b1622a097ad4c86e393461fddba517353
giorgiabes <44198003+giorgiabes@users.noreply.github.com>
2025-06-27 17:54:18 +0400

    modify structure

--- 0001_multipals_of_3_or_5/main.py
+++ 0001_multipals_of_3_or_5/main.py
@@ -6,12 +6,10 @@
 # multiples of 3 or 5 below 1000.
 
 
-def sum_multiples(m1, m2, length):
-    sum = 0
-    for multiple in range(length):
-        if multiple % m1 == 0 or multiple % m2 == 0:
-            sum += multiple
-    return sum
-
-
-print(sum_multiples(3, 5, 1000))
+# solution 1
+# def sum_multiples(m1, m2, length):
+#     sum = 0
+#     for multiple in range(length):
+#         if multiple % m1 == 0 or multiple % m2 == 0:
+#             sum += multiple
+#     return sum
